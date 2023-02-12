dcc37ba7a8a3309f2356e6574c2f3cbc8702d774
Tibor Nagy <Tibor.Nagy@newsignature.com>
2023-02-12 17:49:33 +0200

    Reevaluate on exception

--- TNArch.DistributedConcurrency/TNArch.DistributedConcurrency.Core/Repository/ConcurrencyLockRepository.cs
+++ TNArch.DistributedConcurrency/TNArch.DistributedConcurrency.Core/Repository/ConcurrencyLockRepository.cs
@@ -12,35 +12,35 @@ namespace TNArch.DistributedConcurrency.Core.Repository
             _tableClient = new TableClient(connectionString, nameof(ConcurrencyLock));
         }
 
-        public async Task<bool> AddLock(string lockKey, string unlockKey, DateTime lockExpiration)
+public async Task<bool> AddLock(string lockKey, string unlockKey, DateTime lockExpiration)
+{
+    var locks = await _tableClient.QueryAsync<ConcurrencyLock>(p => p.PartitionKey == "Lock" && p.RowKey == lockKey).ToListAsync();
+
+    var concurrencyLock = locks.FirstOrDefault();
+
+    if (concurrencyLock?.LockExpiration > DateTime.UtcNow && concurrencyLock.UnlockKey != unlockKey)
+        return false;
+
+    try
+    {
+        if (concurrencyLock != null)
         {
-            var locks = await _tableClient.QueryAsync<ConcurrencyLock>(p => p.PartitionKey == "Lock" && p.RowKey == lockKey).ToListAsync();
-
-            var concurrencyLock = locks.FirstOrDefault();
-
-            if (concurrencyLock?.LockExpiration > DateTime.UtcNow && concurrencyLock.UnlockKey != unlockKey)
-                return false;
-
-            try
-            {
-                if (concurrencyLock != null)
-                {
-                    concurrencyLock.LockExpiration = lockExpiration;
-                    concurrencyLock.UnlockKey = unlockKey;
-                    await _tableClient.UpdateEntityAsync(concurrencyLock, concurrencyLock.ETag);
-                }
-                else
-                {
-                    concurrencyLock = new ConcurrencyLock { PartitionKey = "Lock", RowKey = lockKey, LockExpiration = lockExpiration, UnlockKey = unlockKey };
-                    await _tableClient.AddEntityAsync(concurrencyLock);
-                }
-
-                return true;
-            }
-            catch (RequestFailedException ex) when (ex.Status == 409 || ex.Status == 412)
-            {
-                return false;
-            }
+            concurrencyLock.LockExpiration = lockExpiration;
+            concurrencyLock.UnlockKey = unlockKey;
+            await _tableClient.UpdateEntityAsync(concurrencyLock, concurrencyLock.ETag);
         }
+        else
+        {
+            concurrencyLock = new ConcurrencyLock { PartitionKey = "Lock", RowKey = lockKey, LockExpiration = lockExpiration, UnlockKey = unlockKey };
+            await _tableClient.AddEntityAsync(concurrencyLock);
+        }
+
+        return true;
+    }
+    catch (RequestFailedException ex) when (ex.Status == 409 || ex.Status == 412)
+    {
+        return await AddLock(lockKey, unlockKey, lockExpiration);
+    }
+}
     }
 }
